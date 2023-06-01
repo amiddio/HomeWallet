0a910986aa3917738e67c049046b29d169e60b80
d.shkinin <amiddio.dev@gmail.com>
2023-06-01 11:50:39 +0400

    Added language pack

---
+++ lang_pack/lang.py
@@ -0,0 +1,30 @@
+LANG_MENU = {
+    "new": "New",
+    "tags": "Tags",
+    "manage": "Manage",
+    "exit": "Exit",
+}
+
+LANG_GENERAL = {
+    "tags": "Tags",
+    "add": "Add",
+    "save": "Save",
+    "info": "Info",
+    "add tag": "Add tag",
+    "tag name": "Tag name",
+    "validation error": "Validation error",
+    "internal error": "Internal error",
+    "edit": "Edit",
+    "delete": "Delete",
+    "yesno": "?"
+}
+
+LANG_MSG = {
+    "tag '{name}' saved successfully": "Tag '{name}' saved successfully",
+    "are you sure to delete this tag": "Are you sure to delete this tag",
+    "tag '{tag}' deleted successfully": "Tag '{tag}' deleted successfully"
+}
+
+LANG_COLORS = {
+    "header_bg": "#d9d9d9"
+}
